a792cb9f8dd4d2264b165d99899886dfe142ff4e
demirose41 <demirose41@gmail.com>
2024-11-08 00:04:16 -0800

    4. completed phase 4

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const { Player } = require('./game/class/player');
 const { World } = require('./game/class/world');
 
 const worldData = require('./game/data/basic-world-data');
+const { url } = require('inspector');
 
 let player;
 let world = new World();
 world.loadWorld(worldData);
+
+
 
 const server = http.createServer((req, res) => {
 
@@ -56,6 +59,12 @@ const server = http.createServer((req, res) => {
       res.end();
     }
 
+    if (!player){
+      res.statusCode = 302;
+      res.setHeader("Location",`/`)
+      res.end()
+      return 
+    }
     // Phase 3: GET /rooms/:roomId
 
     if (req.method === "GET" && req.url.startsWith("/rooms/")){
@@ -63,7 +72,12 @@ const server = http.createServer((req, res) => {
       if( urlParts.length === 3 ){
         const roomId = urlParts[2];
         const room = world.rooms[roomId];
-        console.log(room)
+        if(room != player.currentRoom){
+          res.statusCode = 302;
+          res.setHeader("Location", `/rooms/${player.currentRoom.id}`)
+          res.end()
+          return
+        }
         const htmlTemplate = fs.readFileSync("./views/room.html",'utf-8');
         const htmlPage = htmlTemplate
         .replace(/#{roomName}/g, room.name)
@@ -73,15 +87,42 @@ const server = http.createServer((req, res) => {
 
         const resBody = htmlPage;
         res.statusCode = 200;
-        res.setHeader("Content-type", 'text/html');
+        res.setHeader("Location", 'text/html');
         res.write(resBody)
         return res.end();
       }
+      // Phase 4: GET /rooms/:roomId/:direction
+      if (urlParts.length === 4){
+        const roomId = urlParts[2];
+        const room = world.rooms[roomId];
+        if(room != player.currentRoom){
+          res.statusCode = 302;
+          res.setHeader("Location", `/rooms/${player.currentRoom.id}`)
+          res.end()
+          return
+        }
+        const direction = urlParts[3][0].toLowerCase();
+        try{
+          player.move(direction)
+          res.statusCode = 302;
+          res.setHeader("Location", `/rooms/${player.currentRoom.id}`)
+          res.end()
+          return
+        } catch (error){
+          // console.error(error)
+          res.statusCode = 302;
+          res.setHeader("Location", `/rooms/${player.currentRoom.id}`);
+          res.end()
+          return
+        }
+
+      }
+
       res.statusCode = 404;
       return res.end()
     }
 
-    // Phase 4: GET /rooms/:roomId/:direction
+    
 
     // Phase 5: POST /items/:itemId/:action
 
